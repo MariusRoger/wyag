4fe11eda7f0cfb2fc2ef85a68a69e8748e82b127
Marius Roger <122625850+MariusRoger@users.noreply.github.com>
2024-10-09 12:38:45 +0200

    feat: add list of commands to main

--- libwyag.py
+++ libwyag.py
@@ -17,10 +17,55 @@ argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
 
 
+def cmd_add(args): ...
+def cmd_cat_file(args): ...
+def cmd_check_ignore(args): ...
+def cmd_checkout(args): ...
+def cmd_commit(args): ...
+def cmd_hash_object(args): ...
+def cmd_init(args): ...
+def cmd_log(args): ...
+def cmd_ls_files(args): ...
+def cmd_ls_tree(args): ...
+def cmd_rev_parse(args): ...
+def cmd_rm(args): ...
+def cmd_show_ref(args): ...
+def cmd_status(args): ...
+def cmd_tag(args): ...
+
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     match args.command:
         case "add":
-            ...
+            cmd_add(args)
+        case "cat-file":
+            cmd_cat_file(args)
+        case "check-ignore":
+            cmd_check_ignore(args)
+        case "checkout":
+            cmd_checkout(args)
+        case "commit":
+            cmd_commit(args)
+        case "hash-object":
+            cmd_hash_object(args)
+        case "init":
+            cmd_init(args)
+        case "log":
+            cmd_log(args)
+        case "ls-files":
+            cmd_ls_files(args)
+        case "ls-tree":
+            cmd_ls_tree(args)
+        case "rev-parse":
+            cmd_rev_parse(args)
+        case "rm":
+            cmd_rm(args)
+        case "show-ref":
+            cmd_show_ref(args)
+        case "status":
+            cmd_status(args)
+        case "tag":
+            cmd_tag(args)
         case _:
-            ...
+            print("Bad command.")
