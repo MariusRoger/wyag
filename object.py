ca51f9ecbe96c0ef937d4253d9abe4587102d995
Marius Roger <122625850+MariusRoger@users.noreply.github.com>
2024-10-13 15:55:55 +0200

    feat(GitObject): handle key-value list with message format

--- object.py
+++ object.py
@@ -1,3 +1,4 @@
+import collections
 import hashlib
 import os
 import zlib
@@ -149,3 +150,85 @@ def object_hash(file_desc, fmt: bytes, repo: GitRepository | None = None) -> str
             raise Exception(f"Unknown type {fmt.decode()}.")
 
     return object_write(obj, repo)
+
+
+def kvlm_parse(
+    raw_object: bytes,
+    start: int = 0,
+    fields_dict: collections.OrderedDict | None = None,
+) -> collections.OrderedDict:
+    """Parse a Key-Value List with Message object into an OrderedDict."""
+    if fields_dict is None:
+        fields_dict = collections.OrderedDict()
+
+    # This function is recursive, so we need to know if we are
+    # at a keyword or already in the message.
+
+    # We search for the next space and the next newline.
+    next_space = raw_object.find(b" ", start)
+    next_newline = raw_object.find(b"\n", start)
+
+    # If space appears before newline, we found a keyword.
+    # Otherwise, it's the final message, which we read until the end of the file.
+
+    # Base case
+    # =========
+    # If newline appears first (or there's no space at all, in which
+    # case find returns -1), we assume a blank line. A blank line
+    # means the remainder of the data is the message. We store it in
+    # the dictionary, with None as the key, and return.
+    if next_space < 0 or next_newline < next_space:
+        assert next_newline == start
+        fields_dict[None] = raw_object[start + 1 :]
+        return fields_dict
+
+    # Recursive case
+    # ==============
+    # we read a key-value pair and recurse for the next.
+    key = raw_object[start:next_space]
+
+    # We loop until we find the end of the value, considering continuation lines
+    # always begin with a space.
+    end = start
+    while end >= 0:
+        end = raw_object.find(b"\n", end + 1)
+        if raw_object[end + 1] != ord(" "):
+            break
+
+    # Get the value and drop leading space in continuation lines.
+    value = raw_object[next_space + 1 : end].replace(b"\n ", b"\n")
+
+    # Don't overwrite existing data contents
+    if key in fields_dict:
+        if type(fields_dict[key]) is list:
+            fields_dict[key].append(value)
+        else:
+            fields_dict[key] = [fields_dict[key], value]
+    else:
+        fields_dict[key] = value
+
+    return kvlm_parse(raw_object, start=end + 1, fields_dict=fields_dict)
+
+
+def kvlm_serialize(fields_dict: collections.OrderedDict):
+    """Serialize a Key-Value List with Message object from an OrderedDict"""
+    output = b""
+
+    # Add output fields
+    for key in fields_dict.keys():
+        # Skip the message
+        if key is None:
+            continue
+        values = fields_dict[key]
+
+        # Normalize to a list
+        if type(values) is list:
+            values = [values]
+
+        for value in values:
+            output += key + b" " + value.replace(b"\n", b"\n ") + b"\n"
+
+    # Append message
+    output += b"\n" + fields_dict[None] + b"\n"
+
+    return output
